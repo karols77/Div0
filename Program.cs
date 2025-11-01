e3a2d60681ddb5da20f30d1ff3b2d023404ab24c
Karol Szewczyk <karols77@o2.pl>
2025-11-01 13:56:34 +0100

    Wersja 1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Div0;
+
+class Program
+{
+    class MyExpetion : Exception { }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Program do wykrywania dzielenia przez 0");
+        try
+        {
+            Console.WriteLine($"3/2 = {ProcedureDiv(3d, 2d)}");
+            Console.WriteLine($"3/1 = {ProcedureDiv(3d, 1d)}");
+            Console.WriteLine($"3/4 = {ProcedureDiv(3d, 4d)}");
+        }
+        catch (MyExpetion)
+        {
+            Console.WriteLine("Wystąpiło dzielenie przez 0!");
+        }
+        finally
+        {
+            Console.WriteLine("Działanie zakończone!!!");
+        }
+    }
+    static double ProcedureDiv(double a, double b)
+    {
+        if (b == 0d)
+        {
+            throw new MyExpetion();
+        }
+        return a / b;
+    }
+}
